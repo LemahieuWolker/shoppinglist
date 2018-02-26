8a818872450506ba713689154a4d2ad535dfc053
LemahieuWolker <wolker.lemahieu@student.howest.be>
2018-02-26 10:06:33 +0100

    Add project files.

---
+++ shoppinglist/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shoppinglist.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
